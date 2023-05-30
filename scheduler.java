990bdee9e4dcefac13fb861943f0271644a6bdc8
danvs6 <wgj6fc@virginia.edu>
2023-05-30 11:56:25 -0400

    implemented schedule class

--- scheduler.java
+++ scheduler.java
@@ -1,54 +1,77 @@
+
 // import Java's array list interface
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-class TimeSlot{
+class TimeSlot { // timeslot class
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
-    //constructor
-    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime){
-        this.startTime=startTime;
-        this.endTime=endTime;
+    // constructor
+    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     // getters and setters
-    public LocalDateTime getStartTime(){
+    public LocalDateTime getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(LocalDateTime startTime){
-        this.startTime=startTime;
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
     }
 
-    public LocalDateTime getEndTime(){
+    public LocalDateTime getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(LocalDateTime endTime){
-        this.endTime=endTime;
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
     }
 }
 
-class Person{ // Person class
+class Person { // Person class
     private String name;
     private List<TimeSlot> availability;
 
-    //getters and setters
-    public String getName(){
+    // getters and setters
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public List<TimeSlot> getAvailability(){
+    public List<TimeSlot> getAvailability() {
         return availability;
     }
 
-    public void setAvailability(List<TimeSlot> availability){
-        this.availability=availability;
+    public void setAvailability(List<TimeSlot> availability) {
+        this.availability = availability;
     }
 }
+
+class Schedule { // schedule class
+    private List<TimeSlot> slots; // stores a list of timeslot objects in a list called slots
+
+    public Schedule() { // constructor; creates a new arraylist with list of time slots
+        this.slots = new ArrayList<>();
+    }
+
+    public void addTimeSlot(TimeSlot timeSlot) { // add a timeslot to the array list
+        slots.add(timeSlot);
+    }
+
+    public void removeTimeSlot(TimeSlot timeSlot) { // remove a timeslot from array list
+        slots.remove(timeSlot);
+    }
+
+    public List<TimeSlot> getSlots() {
+        return slots;
+    }
+
+}
+
